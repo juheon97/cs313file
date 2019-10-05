e1e3b0c0f623f28e9e9def26eb5b3d53acc46a8c
juheon97 <son16007@byui.edu>
2019-10-05 15:54:27 -0600

    prove3 push

---
+++ web/assign3/prove03_01.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="figure.css">
+    <script src = "https://code.jquery.com/jquery-3.3.1.js"></script>   
+    <script src = "jquery.exzoom.js"></script>  
+    <script>
+        $(function(){
+
+            $("#exzoom").exzoom({
+              "autoPlay": false 
+        });
+
+    });
+    </script>
+    <title>Figure01</title>
+</head>
+<body>
+
+<nav>
+        <ul>
+            <li><a href="prove02_02.php">HOME</a></li>
+            <li><a href="test.html">PRODUCTS</a>
+                <ul>
+                    <li><a href="test.html">Figures</a></li>
+                </ul>
+                <li><a href="whatever"><img src='shopping.png' class="logo-image" title="View Cart"></a></li>
+            </li>
+        </ul>
+    </nav>
+    <br>
+    <br>
+    
+   <div class="t_center">
+       <h3>Elder Dragon XL Figure</h3> <p>$ 25.00 </p> <button id="btn1" class="btn btn-info"
+       name="button1"  onclick="addToCart('Elder Drake Figure', 25.00, 1)">Add To Cart</button>
+       <br>
+       <br>
+   </div>
+    <div class="exzoom" id="exzoom">
+        <div class="exzoom_img_box">
+            <ul class='exzoom_img_ul'>
+                 <li><img src="dargon.png" style="float:left"></li>
+            </ul>
+            
+        </div>
+        
+    </div>
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <hr>
+    <footer id = "header-right">
+        <br>
+            <p>Posted by: Juheon Song</p>
+            <p>Contact information: <a href="mailto:juheon6463@gmail.com"><i>juheon6463@gmail.com</i></a>.</p>
+    </footer>
+
+</body>
+</html>
