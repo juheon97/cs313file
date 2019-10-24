f5e81a4ff3f325a35546e3a36190173592b088b1
juheon97 <son16007@byui.edu>
2019-10-24 15:21:18 -0600

    login page

---
+++ web/calendar_project/login_page.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="login_form.css">
+    <title>Loginpage</title>
+</head>
+<body>
+
+    <header>
+        <h1 class="txt_cen">Welcome to the Calendar Project!</h1>
+    </header>
+
+    <form class="login_f">
+        <h1>Login</h1>
+        
+        <div class="txtb">
+            <input type="text">
+            <span data-placeholder="Username"></span>
+        </div>
+
+        <div class="txtb">
+            <input type="password">
+            <span data-placeholder="Password"></span>
+        </div>
+        <input type="submit" class="lgn_but" value="login">
+
+        <div class="acc_btn">
+            <p>Create an Account<p> <a href="create_acc.php">Sign Up</a>
+        <div>
+
+    </form>
+
+    
+</body>
+</html>
