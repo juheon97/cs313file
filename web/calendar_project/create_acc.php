0a1c51fcd6e0d3ae8d2a5828013c8773f753b82b
juheon97 <son16007@byui.edu>
2019-10-25 15:58:06 -0600

    added create.php

---
+++ web/calendar_project/create_acc.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="acc_form.css">
+    <title>Loginpage</title>
+</head>
+<body>
+    <header>
+        <h1 class="txt_cen">Welcome to Calendar Project</h1>
+    </header>
+
+    <form class="login_f">
+        <h1>Create An Account</h1>
+        <div class="txtb">
+            <input type="text" placeholder="First Name">
+        </div>
+
+        <div class="txtb">
+            <input type="text" placeholder="Last Name">
+        </div>    
+        <div class="txtb">
+            <input type="text" placeholder="Username">         
+        </div>
+
+        <div class="txtb">
+            <input type="password" placeholder="Password">
+        </div>
+        <input type="submit" class="lgn_but" value="Create an account">
+
+        <div class="acc_btn">
+            Do you have an account? <a href="login_page.php">Go to Login</a>
+        <div>
+
+    </form>
+
+    
+</body>
+</html>
