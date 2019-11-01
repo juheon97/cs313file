a916ad8201ea299c1ea0d6169877848b60079b21
juheon97 <son16007@byui.edu>
2019-11-01 17:58:20 -0600

    Commit

--- web/calendar_project/calendar_fetch.php
+++ web/calendar_project/calendar_fetch.php
@@ -2,20 +2,17 @@
 require_once("db.php");
 $db = get_db();
 
-$request = $_POST['cal_id'];
+$id_c = $_POST['cal_id'];
 
-echo $request;
-// $id_c = $request[0]['cal_id'];
+$query = 'SELECT sesubject, calendar_id FROM subject_event WHERE calendar_id=:id_c';
+$statement = $db->prepare($query);
+$statement -> bindValue(':id_c', $id_c, PDO::PARAM_INT);
+$statement->execute();
+$sub = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-// $query = 'SELECT sesubject, calendar_id FROM subject_event WHERE calendar_id=:id_c';
-// $statement = $db->prepare($query);
-// $statement -> bindValue(':id_c', $id_c, PDO::PARAM_INT);
-// $statement->execute();
-// $sub = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// $results = '';
-// foreach($sub as $s){
-//     $results .= "<div>Subject:". $s['sesubject'] .", Calendar ID: " .$s['calendar_id'] . "</div>";
-// }
-// echo $results;
+$results = '';
+foreach($sub as $s){
+    $results .= "<div>Subject:". $s['sesubject'] .", Calendar ID: " .$s['calendar_id'] . "</div>";
+}
+echo $results;
 ?>
