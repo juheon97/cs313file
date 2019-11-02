c038dc939d83493fba5c0b19b399154b4fb7c339
juheon97 <son16007@byui.edu>
2019-11-02 14:23:35 -0600

    project commit

--- web/calendar_project/calendar_fetch.php
+++ web/calendar_project/calendar_fetch.php
@@ -5,13 +5,13 @@ $db = get_db();
 $id_f = $_POST['form_id'];
 $query = 'SELECT esubject, edescription, edate, etime, form_id FROM events WHERE form_id=:id_f';
 $statement = $db->prepare($query);
-$statement -> bindValue(':id_c', $id_c, PDO::PARAM_INT);
+$statement -> bindValue(':id_f', $id_f, PDO::PARAM_INT);
 $statement->execute();
 $sub = $statement->fetchAll(PDO::FETCH_ASSOC);
 $results = '';
 
 foreach($sub as $s){
-    $results .=  $s['esubject']." ".$s['edescription']." ".$s['etime']." ".$s['edate']." ";
+    $results .= $s['esubject']." ".$s['edescription']." ".$s['etime']." ".$s['edate']." ";
 }
 echo $results;
 ?>
