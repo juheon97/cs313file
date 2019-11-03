96fa24c967e6ab6f07d9bb03a55888f26b00dd65
juheon97 <son16007@byui.edu>
2019-11-03 15:22:13 -0700

    proejct push

--- web/calendar_project/valid.js
+++ web/calendar_project/valid.js
@@ -9,7 +9,7 @@ function hide(){
 }
 
 
-function valid() {
+function valid(value, id) {
 
     var date = /^((0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2})*$/;
     var validation = "no_error";
