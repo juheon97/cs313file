30333ea6b7f2ab3e27cd2bf50afb2bc792e1038b
juheon97 <son16007@byui.edu>
2019-11-02 20:41:55 -0600

    project commit

--- web/calendar_project/addevent.php
+++ web/calendar_project/addevent.php
@@ -1,7 +1,6 @@
 <?php 
 session_start();
-
-$id2 = $_SESSION['form_id'];
+$id2 = $_SESSION["form_id"];
 $at1 = htmlspecialchars($_POST["ts"]);
 $ad = htmlspecialchars($_POST["td"]);
 $add = htmlspecialchars($_POST["tdd"]);
@@ -23,14 +22,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $stmt_e->bindValue(':ad', $ad, PDO::PARAM_STR);
         $stmt_e->bindValue(':add', $add, PDO::PARAM_STR);
         $stmt_e->bindValue(':att', $att, PDO::PARAM_STR);
-        $stmt_e->bindValue(':id_f', $id2, PDO::PARAM_INT);  
+        $stmt_e->bindValue(':id2', $id2, PDO::PARAM_INT);  
         $resulte = $stmt_e->execute(); 
         header("Location: calendar.php");
     }    
 
 
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -45,33 +43,32 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 <body>
 
-<form class="popup-position" action="" method="POST">
-        <div id="popup-wrapper">
-            <div id="popup-container">
-                    <h3>Add an event</h3>
-                    <div class="txtb">
-                        <input type="text" placeholder="type a subject" name="ts" required />
-                    </div>
-                    <div class="errormessage">
-                        <?= $_SESSION['message8'] ?>
-                    </div>
-                    <div class="txtb">
-                        <input type="text" placeholder="type a description" name="td" required />
-                    </div>
-                    <div class="txtb">
-                        <input type="text" placeholder="tpye a date ex = 10:00:00" name="tt" required />
-                    </div>
-                    <div class="txtb">
-                        <input type="text" placeholder="tpye a date ex = 2019-10-19" name="tdd" required />
-                    </div>
-                    <input type="submit" class="lgn_but" value="Add" name="add_btn">
-            </div>
-        </div>
-    </form>
-
-    <form class="centerize" action="go_calendar.php">
-    <button class='btn btn-warning'>Go back to Calendar</button>
-    </form>
+<form class="login_f" action="" method="POST">
+               
+                            <h3>Add an event</h3>
+                            <div class="txtb">
+                                <input type="text" placeholder="type a subject" name="ts" required />
+                            </div>
+                            <div class="errormessage">
+                                <?= $_SESSION['message8'] ?>
+                            </div>
+                            <div class="txtb">
+                                <input type="text" placeholder="type a description" name="td" required />
+                            </div>
+                            <div class="txtb">
+                                <input type="text" placeholder="tpye a date ex = 10:00:00" name="tt" required />
+                            </div>
+                            <div class="txtb">
+                                <input type="text" placeholder="tpye a date ex = 2019-10-19" name="tdd" required />
+                            </div>
+                            <input type="submit" class="lgn_but" value="Add" name="add_btn">
+                
+            </form>
+        
+            <form  action="go_calendar.php">
+                <div><input type="submit" value="Go back to Calendar" class='btn btn-warning'></div>
+            
+            </form>
 
     
 </body>
